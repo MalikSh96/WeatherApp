98b6bc15f6da9ba54823254b0d5f724acf332f8a
Malik <malik96sharfo@hotmail.com>
2019-05-28 10:07:22 +0200

    having issues running the app

---
+++ app/src/main/java/com/example/weatherapp/Function.kt
@@ -0,0 +1,79 @@
+package com.example.weatherapp
+
+import android.content.Context
+import android.net.ConnectivityManager
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
+
+object Function {
+
+    fun isNetworkAvailable(context: Context): Boolean {
+        return (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo != null
+    }
+
+    fun executeGet(targetURL: String): String? {
+        val url: URL
+        /*lateinit*/ var connection: HttpURLConnection? = null
+        try {
+            //Create connection
+            url = URL(targetURL)
+            connection = url.openConnection() as HttpURLConnection
+            connection.setRequestProperty("content-type", "application/json;  charset=utf-8")
+            connection.setRequestProperty("Content-Language", "en-US")
+            connection.useCaches = false
+            connection.doInput = true
+            connection.doOutput = false
+
+            val inpstr: InputStream
+            val status = connection.responseCode
+            inpstr = if (status != HttpURLConnection.HTTP_OK)
+                connection.errorStream
+            else
+                connection.inputStream
+            val rd = BufferedReader(InputStreamReader(inpstr))
+            val line: String
+            val response = StringBuffer()
+            //issue when converting is here
+            line = rd.readLine()
+            while (line != null) {
+                response.append(line)
+                response.append('\r')
+            }
+            rd.close()
+            return response.toString()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            connection?.disconnect()
+        }
+        return null
+    }
+
+
+    fun setWeatherIcon(actualId: Int, sunrise: Long, sunset: Long): String {
+        val id = actualId / 100
+        var icon = ""
+        if (actualId == 800) {
+            val currentTime = Date().time
+            icon = if (currentTime >= sunrise && currentTime < sunset) {
+                "&#xf00d;"
+            } else {
+                "&#xf02e;"
+            }
+        } else {
+            when (id) {
+                2 -> icon = "&#xf01e;"
+                3 -> icon = "&#xf01c;"
+                7 -> icon = "&#xf014;"
+                8 -> icon = "&#xf013;"
+                6 -> icon = "&#xf01b;"
+                5 -> icon = "&#xf019;"
+            }
+        }
+        return icon
+    }
+}
---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,155 @@
+package com.example.weatherapp
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.graphics.Typeface
+import android.os.AsyncTask
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.Html
+import android.text.Html.fromHtml
+import android.view.View
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import org.json.JSONException
+import org.json.JSONObject
+import java.text.DateFormat
+import java.util.Date
+import java.util.Locale
+
+class MainActivity : AppCompatActivity() {
+    lateinit var selectCity: TextView
+    lateinit var cityField: TextView
+    lateinit var detailsField: TextView
+    lateinit var currentTemperatureField: TextView
+
+    lateinit var humidity_field: TextView
+    lateinit var pressure_field: TextView
+    lateinit var weatherIcon: TextView
+    lateinit var updatedField: TextView
+    lateinit var loader: ProgressBar
+    lateinit var weatherFont: Typeface
+    var city : String = "Barcelona, ES"
+    var OPEN_WEATHER_MAP_API : String = "34910ce547d9588c2b86389c14d95e5c"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        loader = findViewById<View>(R.id.loader) as ProgressBar
+        selectCity = findViewById<View>(R.id.selectCity) as TextView
+        cityField = findViewById<View>(R.id.city_field) as TextView
+        updatedField = findViewById<View>(R.id.updated_field) as TextView
+        detailsField = findViewById<View>(R.id.details_field) as TextView
+        currentTemperatureField = findViewById<View>(R.id.current_temperature_field) as TextView
+        humidity_field = findViewById<View>(R.id.humidity_field) as TextView
+        pressure_field = findViewById<View>(R.id.pressure_field) as TextView
+        weatherIcon = findViewById<View>(R.id.weather_icon) as TextView
+        weatherFont = Typeface.createFromAsset(assets, "fonts/weathericons-regular-webfont.ttf")
+        weatherIcon.typeface = weatherFont
+
+        taskLoadUp(city)
+
+        selectCity.setOnClickListener {
+            print("In selectcity")
+            val alertDialog : AlertDialog.Builder  = AlertDialog.Builder(this@MainActivity)
+            alertDialog.setTitle("Change City")
+            val input = EditText(this@MainActivity)
+            input.setText(city)
+            val lp = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+            )
+            input.layoutParams = lp
+            alertDialog.setView(input)
+
+            alertDialog.setPositiveButton(
+                "Change"
+            ) { _ : DialogInterface, _ : Int ->
+                city = input.text.toString()
+                taskLoadUp(city)
+            }
+            alertDialog.setNegativeButton("Cancel"
+            ) { dialog : DialogInterface, _ : Int -> dialog.cancel() }
+            alertDialog.show()
+        }
+
+    }
+
+
+    fun taskLoadUp(query: String) {
+        if (Function.isNetworkAvailable(applicationContext)) {
+            val task = DownloadWeather()
+            task.execute(query)
+        } else {
+            Toast.makeText(applicationContext, "No Internet Connection", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    inner class DownloadWeather : AsyncTask<String, Void, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            loader.visibility = View.VISIBLE
+
+        }
+
+        override fun doInBackground(vararg args : /*Array<String>*/ String): String? {
+            return Function.executeGet("http://api.openweathermap.org/data/2.5/weather?q=" + args[0] +
+                    "&units=metric&appid=" + OPEN_WEATHER_MAP_API
+            )
+        }
+
+        override fun onPostExecute(xml: String) {
+            try {
+                val json = JSONObject(xml)
+                if (json != null) {
+                    val details = json.getJSONArray("weather").getJSONObject(0)
+                    val main = json.getJSONObject("main")
+                    val df = DateFormat.getDateTimeInstance()
+
+                    cityField.text =
+                        json.getString("name").toUpperCase(Locale.US) + ", " + json.getJSONObject("sys").getString("country")
+                    detailsField.text = details.getString("description").toUpperCase(Locale.US)
+                    currentTemperatureField.text = String.format("%.2f", main.getDouble("temp")) + "°"
+                    humidity_field.text = "Humidity: " + main.getString("humidity") + "%"
+                    pressure_field.text = "Pressure: " + main.getString("pressure") + " hPa"
+                    updatedField.text = df.format(Date(json.getLong("dt") * 1000))
+                    weatherIcon.text = Html.fromHtml(
+                        Function.setWeatherIcon(
+                            details.getInt("id"),
+                            json.getJSONObject("sys").getLong("sunrise") * 1000,
+                            json.getJSONObject("sys").getLong("sunset") * 1000
+                        )
+                    )
+
+                    loader.visibility = View.GONE
+
+                    /*cityField.text = json.getString("name").toUpperCase(Locale.US) + ", " + json.getJSONObject("sys").getString(
+                        "country")
+                    detailsField.text = details.getString("description").toUpperCase(Locale.US)
+                    currentTemperatureField.text = String.format("%.2f", main.getDouble("temp")) + "°"
+                    humidity_field.text = "Humidity: " + main.getString("humidity") + "%"
+                    pressure_field.text = "Pressure: " + main.getString("pressure") + " hPa"
+                    updatedField.text = df.format(Date(json.getLong("dt") * 1000))
+                    weatherIcon.text = Html.fromHtml(
+                        Function.setWeatherIcon(
+                            details.getInt("id"),
+                            json.getJSONObject("sys").getLong("sunrise") * 1000,
+                            json.getJSONObject("sys").getLong("sunset") * 1000
+                        ))
+
+                    loader.visibility = View.GONE*/
+
+                }
+            } catch (e: JSONException) {
+                Toast.makeText(applicationContext, "Error, Check City", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
